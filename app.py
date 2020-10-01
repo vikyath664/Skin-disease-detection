132c242ceeeaa84665c3107e32664aab32ead7cd
vikyath664 <49971328+vikyath664@users.noreply.github.com>
2020-10-01 15:36:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import pyrebase
+
+config ={
+  "apiKey": "AIzaSyCM8-SvGWAQb5s5p5BmlHAFnZo0jJqJBRY",
+  "authDomain": "webauth-aad13.firebaseapp.com",
+  "databaseURL": "https://webauth-aad13.firebaseio.com",
+  "projectId": "webauth-aad13",
+  "storageBucket": "webauth-aad13.appspot.com",
+  "messagingSenderId": "731344591097",
+  "appId": "1:731344591097:web:e453efb8fb4db03e98aa28",
+  "measurementId": "G-ZLN48LN7S9"
+}
